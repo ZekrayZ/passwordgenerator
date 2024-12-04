f8bed09178ab1c14671aeb53db6bd1bbf6db723e
Zekray <power.omar123@gmail.com>
2024-12-04 10:40:33 -0400

    Avance sobre la longitud de la password

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,4 +1,6 @@
-print('Hola putito')
-print("si")
-#Hola
-#no
+# Eleccion de longitud de la contraseña
+Long = int(input('Cuantos caracteres quiere en la contraseña?: '))
+
+# Dentro de aqui creo que deberia ir todo el programa por tema de que es un buclea
+for i in range(Long):
+    print('')
